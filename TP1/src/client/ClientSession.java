c12fea03044ff2d4e914c7e45fb096f7abad27b4
Rafael-LN <rafaelnunes3092@gmail.com>
2025-04-27 18:38:19 +0100

    Add photoBase64 field and accessors to ClientSession for user profile management

--- TP1/src/client/ClientSession.java
+++ TP1/src/client/ClientSession.java
@@ -1,8 +1,10 @@
 package client;
 
 public class ClientSession {
+
     private String username;
     private boolean loggedIn;
+    private String photoBase64;
 
     public void login(String username) {
         this.username = username;
@@ -12,9 +14,22 @@ public class ClientSession {
     public void logout() {
         this.username = null;
         this.loggedIn = false;
+        this.photoBase64 = null;
     }
 
+    public String getUsername() {
+        return username;
+    }
 
-    public String getUsername() { return username; }
-    public boolean isLoggedIn() { return loggedIn; }
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public String getPhotoBase64() {
+        return photoBase64;
+    }
+
+    public void setPhotoBase64(String photoBase64) {
+        this.photoBase64 = photoBase64;
+    }
 }
